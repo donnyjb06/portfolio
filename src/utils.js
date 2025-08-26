f3cf212d71216de97789fda439ea1d9f1f1ddc8e
Donovan Jabbar <djabbar51@gmail.com>
2025-08-25 21:45:57 -0500

    chore: remove vite/react template code

---
+++ src/utils.js
@@ -0,0 +1,9 @@
+import { RiTailwindCssFill, BiLogoTypescript, RiNextjsFill, SiMongodb, RiReactjsFill, IoLogoSass, SiJest, SiExpress  } from "react-icons/ri";
+
+const generateTechIcons = (theme) => {
+  return [
+    {language: "Typescript", icon: < BiLogoTypescript color=/>}
+  ] 
+}
+
+export { generateTechIcons }
