c44ecb7290dce54004e0ece614b4119109a57a9d
Donovan Jabbar <djabbar51@gmail.com>
2025-08-27 21:08:30 -0500

    feat(GridContainer): add relative positioning

--- src/components/GridContainer.tsx
+++ src/components/GridContainer.tsx
@@ -2,7 +2,7 @@ import type { Props } from "../types/ui";
 
 const GridContainer = ({children}: Props) => {
 	return (
-		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:grid-rows-[276px_100px_auto_168px] gap-4 md:gap-5 lg:gap-6 max-w-screen-xl h-full lg:max-h-[800px] mx-auto ">
+		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 lg:grid-rows-[276px_100px_auto_168px] gap-4 md:gap-5 lg:gap-6 max-w-screen-xl h-full lg:max-h-[800px] mx-auto relative">
 			{children}
 		</div>
 	);
