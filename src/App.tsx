2751f3630fa6d1472f167e6ca96867135dff7697
Donovan Jabbar <djabbar51@gmail.com>
2025-08-30 06:46:23 -0500

    build(App): add insights components

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import Projects from "./components/Projects";
 import Certifications from "./components/Certifications";
 import Connect from "./components/Connect";
 import CustomCursor from "./components/CustomCursor";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const CELLS = [
 	Summary,
@@ -21,6 +23,8 @@ const CELLS = [
 function App() {
 	return (
 		<ThemeProvider>
+			<Analytics />
+			<SpeedInsights />
 			<CustomCursor />
 			<StarsBackground className="fixed inset-0 flex items-center justify-center min-h-full" />
 			<GridContainer>
